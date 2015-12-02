de87e83510fd572775072bddb7a105bbab794219
Conrad Dandelski <conrad.dandelski@mycit.ie>
2015-12-02 15:16:33 +0000

    added common files for CIDER

---
+++ DEWI/src/networklayer/CIDERNet/CIDER/common/CIDERControlInfo_m.cc
@@ -0,0 +1,262 @@
+//
+// Generated file, do not edit! Created by nedtool 4.6 from src/networklayer/CIDERNet/CIDER/common/CIDERControlInfo.msg.
+//
+
+// Disable warnings about unused variables, empty switch stmts, etc:
+#ifdef _MSC_VER
+#  pragma warning(disable:4101)
+#  pragma warning(disable:4065)
+#endif
+
+#include <iostream>
+#include <sstream>
+#include "CIDERControlInfo_m.h"
+
+USING_NAMESPACE
+
+
+// Another default rule (prevents compiler from choosing base class' doPacking())
+template<typename T>
+void doPacking(cCommBuffer *, T& t) {
+    throw cRuntimeError("Parsim error: no doPacking() function for type %s or its base class (check .msg and _m.cc/h files!)",opp_typename(typeid(t)));
+}
+
+template<typename T>
+void doUnpacking(cCommBuffer *, T& t) {
+    throw cRuntimeError("Parsim error: no doUnpacking() function for type %s or its base class (check .msg and _m.cc/h files!)",opp_typename(typeid(t)));
+}
+
+
+
+
+// Template rule for outputting std::vector<T> types
+template<typename T, typename A>
+inline std::ostream& operator<<(std::ostream& out, const std::vector<T,A>& vec)
+{
+    out.put('{');
+    for(typename std::vector<T,A>::const_iterator it = vec.begin(); it != vec.end(); ++it)
+    {
+        if (it != vec.begin()) {
+            out.put(','); out.put(' ');
+        }
+        out << *it;
+    }
+    out.put('}');
+    
+    char buf[32];
+    sprintf(buf, " (size=%u)", (unsigned int)vec.size());
+    out.write(buf, strlen(buf));
+    return out;
+}
+
+// Template rule which fires if a struct or class doesn't have operator<<
+template<typename T>
+inline std::ostream& operator<<(std::ostream& out,const T&) {return out;}
+
+Register_Class(CIDERControlInfo);
+
+CIDERControlInfo::CIDERControlInfo(const char *name, int kind) : ::cMessage(name,kind)
+{
+}
+
+CIDERControlInfo::CIDERControlInfo(const CIDERControlInfo& other) : ::cMessage(other)
+{
+    copy(other);
+}
+
+CIDERControlInfo::~CIDERControlInfo()
+{
+}
+
+CIDERControlInfo& CIDERControlInfo::operator=(const CIDERControlInfo& other)
+{
+    if (this==&other) return *this;
+    ::cMessage::operator=(other);
+    copy(other);
+    return *this;
+}
+
+void CIDERControlInfo::copy(const CIDERControlInfo& other)
+{
+}
+
+void CIDERControlInfo::parsimPack(cCommBuffer *b)
+{
+    ::cMessage::parsimPack(b);
+}
+
+void CIDERControlInfo::parsimUnpack(cCommBuffer *b)
+{
+    ::cMessage::parsimUnpack(b);
+}
+
+class CIDERControlInfoDescriptor : public cClassDescriptor
+{
+  public:
+    CIDERControlInfoDescriptor();
+    virtual ~CIDERControlInfoDescriptor();
+
+    virtual bool doesSupport(cObject *obj) const;
+    virtual const char *getProperty(const char *propertyname) const;
+    virtual int getFieldCount(void *object) const;
+    virtual const char *getFieldName(void *object, int field) const;
+    virtual int findField(void *object, const char *fieldName) const;
+    virtual unsigned int getFieldTypeFlags(void *object, int field) const;
+    virtual const char *getFieldTypeString(void *object, int field) const;
+    virtual const char *getFieldProperty(void *object, int field, const char *propertyname) const;
+    virtual int getArraySize(void *object, int field) const;
+
+    virtual std::string getFieldAsString(void *object, int field, int i) const;
+    virtual bool setFieldAsString(void *object, int field, int i, const char *value) const;
+
+    virtual const char *getFieldStructName(void *object, int field) const;
+    virtual void *getFieldStructPointer(void *object, int field, int i) const;
+};
+
+Register_ClassDescriptor(CIDERControlInfoDescriptor);
+
+CIDERControlInfoDescriptor::CIDERControlInfoDescriptor() : cClassDescriptor("CIDERControlInfo", "cMessage")
+{
+}
+
+CIDERControlInfoDescriptor::~CIDERControlInfoDescriptor()
+{
+}
+
+bool CIDERControlInfoDescriptor::doesSupport(cObject *obj) const
+{
+    return dynamic_cast<CIDERControlInfo *>(obj)!=NULL;
+}
+
+const char *CIDERControlInfoDescriptor::getProperty(const char *propertyname) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? basedesc->getProperty(propertyname) : NULL;
+}
+
+int CIDERControlInfoDescriptor::getFieldCount(void *object) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? 0+basedesc->getFieldCount(object) : 0;
+}
+
+unsigned int CIDERControlInfoDescriptor::getFieldTypeFlags(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldTypeFlags(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    return 0;
+}
+
+const char *CIDERControlInfoDescriptor::getFieldName(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldName(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    return NULL;
+}
+
+int CIDERControlInfoDescriptor::findField(void *object, const char *fieldName) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? basedesc->findField(object, fieldName) : -1;
+}
+
+const char *CIDERControlInfoDescriptor::getFieldTypeString(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldTypeString(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    return NULL;
+}
+
+const char *CIDERControlInfoDescriptor::getFieldProperty(void *object, int field, const char *propertyname) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldProperty(object, field, propertyname);
+        field -= basedesc->getFieldCount(object);
+    }
+    switch (field) {
+        default: return NULL;
+    }
+}
+
+int CIDERControlInfoDescriptor::getArraySize(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getArraySize(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    CIDERControlInfo *pp = (CIDERControlInfo *)object; (void)pp;
+    switch (field) {
+        default: return 0;
+    }
+}
+
+std::string CIDERControlInfoDescriptor::getFieldAsString(void *object, int field, int i) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldAsString(object,field,i);
+        field -= basedesc->getFieldCount(object);
+    }
+    CIDERControlInfo *pp = (CIDERControlInfo *)object; (void)pp;
+    switch (field) {
+        default: return "";
+    }
+}
+
+bool CIDERControlInfoDescriptor::setFieldAsString(void *object, int field, int i, const char *value) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->setFieldAsString(object,field,i,value);
+        field -= basedesc->getFieldCount(object);
+    }
+    CIDERControlInfo *pp = (CIDERControlInfo *)object; (void)pp;
+    switch (field) {
+        default: return false;
+    }
+}
+
+const char *CIDERControlInfoDescriptor::getFieldStructName(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldStructName(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    return NULL;
+}
+
+void *CIDERControlInfoDescriptor::getFieldStructPointer(void *object, int field, int i) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldStructPointer(object, field, i);
+        field -= basedesc->getFieldCount(object);
+    }
+    CIDERControlInfo *pp = (CIDERControlInfo *)object; (void)pp;
+    switch (field) {
+        default: return NULL;
+    }
+}
+
+
