6cc7987888aedc82fdc60718450ee31993fbe872
Conrad <conrad.dandelski@mycit.ie>
2016-02-25 09:27:27 +0000

    small changes

--- DEWI/src/networklayer/CIDERNet/CIDER/CIDER.cc
+++ DEWI/src/networklayer/CIDERNet/CIDER/CIDER.cc
@@ -13,7 +13,7 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#include <CIDER/CIDER.h>
+#include "CIDER.h"
 #include "CIDERControlInfo_m.h"
 #include "CIDERFrame_m.h"
 
